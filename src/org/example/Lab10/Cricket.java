420a1c2eca25eefe601d310f5ea67efe4dca6e7c
Yerkhan <231132@astanait.edu.kz>
2024-11-01 22:54:58 +0500

    lab 10

---
+++ src/org/example/Lab10/Cricket.java
@@ -0,0 +1,18 @@
+package org.example.Lab10;
+
+class Cricket extends Game {
+
+    void initialize() {
+        System.out.println("Cricket Game Initialized! Start playing.");
+    }
+
+
+    void startPlay() {
+        System.out.println("Cricket Game Started. Enjoy the game!");
+    }
+
+
+    void endPlay() {
+        System.out.println("Cricket Game Finished!");
+    }
+}
