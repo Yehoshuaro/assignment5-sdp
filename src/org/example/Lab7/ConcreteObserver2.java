17ce5156ee93ace651eaac14ebd9f6650446708a
Yerkhan <231132@astanait.edu.kz>
2024-11-01 22:26:21 +0500

    lab 7

---
+++ src/org/example/Lab7/ConcreteObserver2.java
@@ -0,0 +1,8 @@
+package org.example.Lab7;
+
+class ConcreteObserver2 implements Observer {
+
+    public void update(String message) {
+        System.out.println("Observer 2 received update: " + message);
+    }
+}
---
+++ src/org/example/Lab7/Observer.java
@@ -0,0 +1,5 @@
+package org.example.Lab7;
+
+interface Observer {
+    void update(String message);
+}
